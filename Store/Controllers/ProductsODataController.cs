2b39546020b0ea61a76a5bbc91e007906857ec04
Hassan Rezk <harezk@microsoft.com>
2019-09-23 08:15:49 -0700

    Add project files.

---
+++ Store/Controllers/ProductsODataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+using Store.Services;
+
+namespace Store.Controllers
+{
+    [Route("odata/products")]
+    public class ProductsODataController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [EnableQuery()]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return new ProductService().GetProducts();
+        }
+    }
+}
---
+++ Store/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Store.Models;
+
+namespace Store.Services
+{
+    public class ProductService
+    {
+        public List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Milk",
+                    Price = 10
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Chocolate",
+                    Price = 5
+                }
+            };
+        }
+    }
+}
